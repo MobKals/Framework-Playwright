336851dcfa5b2173af2cb95f33c47344402a5fe3
Kalyani Nam <kalyanin@mobiquityinc.com>
2022-09-20 17:06:53 +0200

    First Commit

---
+++ tests/Register.spec.js
@@ -0,0 +1,16 @@
+const {test,expect} = require('@playwright/test');
+
+
+test('Your Logo login Test', async({browser})=>
+{
+    //chrome - plugin/cookies
+        const context = await browser.newContext(); //browser instance is open without cookies
+        const page = await context.newPage(); // creates actual page to start your automation
+        await page.goto("http://automationpractice.com/index.php");
+        await page.locator(".login").click();
+        await page.waitForLoadState('networkidle');
+        await page.locator("#email_create").type("kalyanin@mobiquityinc.com");
+        await page.locator("button[id='SubmitCreate'] span").click();
+        await page.waitForLoadState('networkidle');
+        
+});
